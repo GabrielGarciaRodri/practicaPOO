4e6d6354eba53fd051ffda3a424ff34f7ffb99b2
GabrielGarciaRodri <145493679+GabrielGarciaRodri@users.noreply.github.com>
2024-06-05 12:28:42 -0500

    feat: :construction: Juego.js needs to be fixed
    Juego.js is presenting a error and needs to be fixed but Im late for Campus so I'll do it later

--- js/Juego.js
+++ js/Juego.js
@@ -1,7 +1,11 @@
+
+import { Monstruo } from "./Monstruo.js ";
+
 export class Juego {
     constructor(){
         this.historial = [];
         this.logLevel = "info"; //Login principal pero no se para que por ahora (no olvidar) xd
+        this.monstruo = null;
     }
 
     iniciarJuego(){
@@ -29,5 +33,50 @@ export class Juego {
             this.agregarEventoAlHistorial(mensaje); 
             console.log(mensaje);
         }
+    }
+
+    //Mostrar monstuo
+    
+    setMonstruo(){
+        this.monstruo = monstruo;
+        console.log(`El monstruo es ${this.monstruo.nombre}`);
+    }
+
+    investigar(){
+        const monstruos = [
+            function crearGoblin(){
+                return new Monstruo("Goblin", 50, 75, 15, 8);
+            },
+            
+            function crearOrco(){
+                return new Monstruo("Orco", 100, 100, 20, 10);
+            },
+
+            function crearTroll(){
+                return new Monstruo("Troll", 150, 150, 25, 15);
+            },
+
+            function crearDragon(){
+                return new Monstruo("Dragon", 200, 200, 30, 20);
+            },
+
+            function crearOgro(){
+                return new Monstruo("Ogro", 150, 150, 25, 15);
+            },
+
+            function crearGigante(){
+                return new Monstruo("Gigante", 200, 200, 30, 20);
+            },
+
+            function crearMago(){
+                return new Monstruo("Mago", 150, 150, 25, 15);
+            },
+        ];
+
+        const randomIndex = Math.floor(Math.random() * monstruos.length);
+        const nuevoMonstruo = new monstruos[randomIndex]();
+        this.setMonstruo(nuevoMonstruo);
+        Juego.logear(`Un nuevo monstruo a aparecido!!! Es un: ${nuevoMonstruo.nombre}`, "info");
+
     }
 }
--- js/main.js
+++ js/main.js
@@ -3,23 +3,27 @@ import { Monstruo } from "./Monstruo.js";
 import {Juego} from "./Juego.js";
 
 
-// const heroe = new Heroe("Valiente", 100, 100, 80, 50, "Curacion");
-// const monstruo = new Monstruo("Golem", 200, 150, 60, 70, "Tierra");
+const heroe = new Heroe("Valiente", 100, 100, 80, 50, "Curacion");
+const monstruo = new Monstruo("Golem", 200, 150, 60, 70, "Tierra");
 
-// heroe.atacar(monstruo);
-// monstruo.rugir();
-// heroe.usarHabilidadEspecial(monstruo);
-// monstruo.atacar(heroe);
-// monstruo.atacar(heroe);
-// heroe.mostrarEstado();
-// monstruo.mostrarEstado();
+heroe.atacar(monstruo);
+monstruo.rugir();
+heroe.usarHabilidadEspecial(monstruo);
+monstruo.atacar(heroe);
+monstruo.atacar(heroe);
+heroe.mostrarEstado();
+monstruo.mostrarEstado();
 
 const juego = new Juego();
 
+
 juego.iniciarJuego();
+
+juego.investigar(); //Crea monstruo random uwu
 
 juego.agregarEventoAlHistorial("El héroe ataca al monstruo.");
 juego.agregarEventoAlHistorial("El monstruo ruge y contraataca.");
 juego.agregarEventoAlHistorial("El héroe utiliza su habilidad especial.");
 
+juego.setMonstruo(monstruo);
 juego.mostrarHistorial();
