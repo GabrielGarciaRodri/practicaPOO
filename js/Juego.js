5e523f51cc8f57bb6d56516f1339979c00904487
GabrielGarciaRodri <145493679+GabrielGarciaRodri@users.noreply.github.com>
2024-06-05 11:27:16 -0500

    feat: :sparkles: File juego.js created
    This file is used to save classes and methods to control the game in general

---
+++ js/Juego.js
@@ -0,0 +1,25 @@
+export class Juego {
+    constructor(){
+        this.historial = [];
+    }
+
+    iniciarJuego(){
+        console.log("Iniciando el juego");
+    }
+
+    agregarEventoAlHistorial(evento){
+        this.historial.push(evento);
+        console.log(evento);
+    }
+
+    mostrarHistorial(){
+        console.log("**HISTORIAL**")
+        if(this.historial.length === 0 ){
+            console.log("El historial está vacío");
+        } else {
+            for(const evento of this.historial){
+                console.log(evento);
+            }
+        }
+    }
+}
