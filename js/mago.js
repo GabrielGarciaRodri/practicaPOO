3a037d3b0a5191be25f168bcdf4e199faf288b45
GabrielGarciaRodri <145493679+GabrielGarciaRodri@users.noreply.github.com>
2024-06-05 11:26:41 -0500

    feat: :fire: Mago.js is no longer usefull
    This file is not useful now so it's better to continue without this file

--- js/mago.js
+++
@@ -1,8 +0,0 @@
-import { personaje } from "./personaje.js"
-export class mago extends personaje{
-    constructor(nom){
-        super();
-        this.nombre = nom;
-        this.setVida = 10;
-    }
-}
